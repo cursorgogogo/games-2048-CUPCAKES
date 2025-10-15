34114432782ab1d591211e0bb89496b49b37d23b
aspower <itysh8@gmail.com>
2025-10-15 21:27:10 +0800

    feat:修改语言正确性

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -172,11 +172,11 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
   tweet.setAttribute("data-via", "giampiex");
-  tweet.setAttribute("data-url", "http://git.io/cupcakes");
-  tweet.setAttribute("data-counturl", "http://0x0800.github.io/2048-CUPCAKES");
+  tweet.setAttribute("data-url", "https://2048club.com");
+  tweet.setAttribute("data-counturl", "https://2048club.com");
   tweet.textContent = "Tweet";
 
-  var text = Localize("tweet1") + Localize( this.score ).toUpperCase() + '", ' + this.points + " Kcal " + Localize("tweet2");
+  var text = Localize("tweet1") + Localize( "p" + this.score ) + '", ' + this.points + " Kcal " + Localize("tweet2");
   tweet.setAttribute("data-text", text);
 
   return tweet;
